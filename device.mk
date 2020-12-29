754554a73c0655cbe548dfef23be22969fd49855
althafvly <althafvly@gmail.com>
2020-12-29 20:34:33 +0530

    dipper: Build libavservices_minijail vendor variant
    Change-Id: I52c67c4abe0eaa62ce31f64e7b362182732855d9

--- device.mk
+++ device.mk
@@ -208,6 +208,7 @@ PRODUCT_PACKAGES += \
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
+    libavservices_minijail.vendor \
     libc2dcolorconvert \
     libOmxAacEnc \
     libOmxAmrEnc \
