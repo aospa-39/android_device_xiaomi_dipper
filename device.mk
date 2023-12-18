8ba76ead3cd7df24cbbaa934743871b6a73ae8f8
sashascurtu <sasha.scurtu@gmail.com>
2023-12-17 23:45:22 -0500

    dipper: Build sensorservice 1.0 HAL
    Fixes:
    W HidlServiceManagement: Waited one second for android.frameworks.sensorservice@1.0::ISensorManager/default
    I hwservicemanager: Since android.frameworks.sensorservice@1.0::ISensorManager/default is not registered, trying to start it as a lazy HAL.
    I HidlServiceManagement: getService: Trying again for android.frameworks.sensorservice@1.0::ISensorManager/default...
    W libc    : Unable to set property ctl.interface_start to android.frameworks.sensorservice@1.0::ISensorManager/default: error code: 0x20
    
    Change-Id: If6fvdef9dc39daaba9138fe3f8acf06cb6a0e78f8

--- device.mk
+++ device.mk
@@ -495,6 +495,8 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0 \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@1.0-impl:64 \
     android.hardware.sensors@1.0-service \
     libsensorndkbridge
