a938314d2eb8c11475021336afff1518247d713d
Karan Parashar <karan@pixelos.net>
2024-08-03 22:30:04 +0530

    dipper: Build libutils.vendor
    E vndksupport: Could not load libutils.so from sphal namespace: dlopen failed: library "libutils.so" not found.
    
    Change-Id: I5bd9623fa7a25efd5c429bac3eca78e10855219e

--- device.mk
+++ device.mk
@@ -422,6 +422,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
 # VNDK
+PRODUCT_PACKAGES += \
+    libutils.vendor
+
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v33.so
 
