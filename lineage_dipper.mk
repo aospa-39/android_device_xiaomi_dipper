0a1a281259b8f34c313f7b6b3923ffe8a4210d3f
xiaoleGun <1592501605@qq.com>
2024-09-17 11:23:32 -0300

    sdm845-common: Decommonize device tree
    Change-Id: Ie7cf33e41bd9a94418c0b2272df500dd00dab903
    Co-authored-by: Vhmit <viktor.9630@protonmail.com>
    Co-authored-by: SH4ND <miqueiasantos20@gmail.com>

---
+++ lineage_dipper.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2018-2019,2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+# Inherit from device
+$(call inherit-product, device/xiaomi/dipper/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := lineage_dipper
+PRODUCT_DEVICE := dipper
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := MI 8
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_SYSTEM_NAME := dipper
+
+BUILD_FINGERPRINT := "Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys" \
+    TARGET_PRODUCT="dipper"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
