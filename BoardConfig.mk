acc7faefbc1e2a6cc1c67cf600f1b8f77a5d0455
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-11-25 00:02:28 +0000

    dipper: Switch to hardware/xiaomi FCM
    Change-Id: I9388248bebaad2e04cda300b1d45b1f24401c65f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,7 +47,7 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
 
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-     $(DEVICE_PATH)/configs/vintf/xiaomi_vendor_framework_compatibility_matrix.xml
+     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 
 # Kernel
 BOARD_KERNEL_PAGESIZE := 4096
