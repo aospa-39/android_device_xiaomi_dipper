d0071bef8a97650fe4fc27319936992f93401ace
xiaoleGun <1592501605@qq.com>
2025-04-04 14:35:02 +0800

    dipper: Add device framework matrix
    Change-Id: I5d5d9733ff1f310a2e2061cc7d4f7065bc279bd2
    Signed-off-by: xiaoleGun <1592501605@qq.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -59,7 +59,8 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
-    vendor/aosp/config/device_framework_matrix.xml
+    vendor/aosp/config/device_framework_matrix.xml \
+    $(DEVICE_PATH)/configs/vintf/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 
